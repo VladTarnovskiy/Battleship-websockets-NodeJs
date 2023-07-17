cfde1d259fec4d295417934d4976a245929553a1
Vlad Tarnovski <vladtarnovski989@gmail.com>
2023-07-18 01:20:43 +0300

    feat: interfaces

--- src/interfaces/interfaces.ts
+++ src/interfaces/interfaces.ts
@@ -1,11 +1,61 @@
 import { WebSocket } from "ws";
 
-export interface RequestData {
+export type RequestBody = {
   type: string;
   data: string;
-  id: number;
-}
+  id: 0;
+};
+
+export type ResRegData = {
+  name: string;
+  index: number;
+  error: boolean;
+  errorText: string;
+};
 
 export interface MyWebSocket extends WebSocket {
-  index: string;
+  id: string;
 }
+
+export interface Player {
+  name: string;
+  password: string;
+  index: number;
+  active: boolean;
+  isBot: boolean;
+}
+
+export type Room = {
+  roomId: number;
+  roomUsers: Omit<Player, "password" | "active">[];
+};
+
+export type Ship = {
+  position: {
+    x: number;
+    y: number;
+  };
+  direction: boolean;
+  length: number;
+  type: "small" | "medium" | "large" | "huge";
+};
+
+export type Winner = { name: Player["name"]; wins: number };
+
+export type ShipField = {
+  killed: boolean;
+  positions: { x: number; y: number; status: boolean }[];
+}[];
+
+export type ActiveGame = {
+  finished: boolean;
+  turn: number;
+  gameId: number;
+  sentShipsCounter: 0 | 1 | 2;
+  players: {
+    index: number;
+    isBot?: boolean;
+    shipField: ShipField;
+    hittedFields: { x: number; y: number }[];
+  }[];
+};
---
+++ src/interfaces/response.ts
@@ -0,0 +1,62 @@
+import { Ship } from "./interfaces";
+
+export type ResponseBody = {
+  type: string;
+  data: string;
+  id: 0;
+};
+
+export type ResData =
+  | ResRegData
+  | ResUpdateRoomData
+  | ResUpdateWinnersData
+  | ResUpdateRoomData
+  | ResCreateGameData
+  | ResStartGameData
+  | ResAttackData
+  | ResTurnData
+  | ResFinishData;
+
+export type ResRegData = {
+  name: string;
+  index: number;
+  error: boolean;
+  errorText: string;
+};
+
+export type ResUpdateWinnersData = {
+  name: string;
+  wins: number;
+}[];
+
+export type ResUpdateRoomData = {
+  roomId: number;
+  roomUsers: { name: string; index: number }[];
+}[];
+
+export type ResCreateGameData = {
+  idGame: number;
+  idPlayer: number;
+};
+
+export type ResStartGameData = {
+  ships: Ship[];
+  currentPlayerIndex: number;
+};
+
+export type ResAttackData = {
+  position: {
+    x: number;
+    y: number;
+  };
+  currentPlayer: number;
+  status: "miss" | "killed" | "shot";
+};
+
+export type ResTurnData = {
+  currentPlayer: number;
+};
+
+export type ResFinishData = {
+  winPlayer: number;
+};
